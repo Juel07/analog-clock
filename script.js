c58432f6abd1d4f13e35d839f60febc055a42543
Juel B <juel.braganza97@gmail.com>
2021-01-31 01:26:33 +0000

    made hands rotate + changed const to let

--- script.js
+++ script.js
@@ -9,22 +9,28 @@ const displayTime = () => {
     // get the current time - hour, mins, seconds
     let time = new Date()
 
-    const secs = time.getSeconds()
-    const mins = time.getMinutes()
-    const hours = time.getHours()
+    let hours = time.getHours()
+    let mins = time.getMinutes()
+    let secs = time.getSeconds()
 
     // convert the time to a degree
     const secsDegree = (360 * secs) / 60
     const minsDegree = (360 * mins) / 60
     const hoursDegree = (360 * hours) / 60
 
+    // make the hands rotate as per degree position 
+    hourHand.style.transform = `rotate(${hoursDegree}deg)`
+    minHand.style.transform = `rotate(${minsDegree}deg)`
+    secHand.style.transform = `rotate(${secsDegree}deg)`
+
     // display time on digital clock
-    if (hours <= 0) { hours = '0' + hours } else { hours }
-    if (secs < 10) { secs = '0' + secs } else { secs }
-    if (mins < 10) { mins = '0' + mins } else { mins }
+    if (hours < 10) { hours = '0' + hours }
+    if (mins < 10) { mins = '0' + mins }
+    if (secs < 10) { secs = '0' + secs }
 
     digitalTime.innerText = `${hours}:${mins}:${secs}`
 }
 
 // update the time every 1000ms / 1s
 setInterval(displayTime, 1000)
+
